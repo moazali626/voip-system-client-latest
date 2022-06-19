610f8b3f14c0e18de232ee7bb975dc28a86b601f
Moaz <moazali626@gmail.com>
2022-06-19 21:17:29 +0500

    receive a call is implemented, just need few refactoring

--- src/components/Modules/ReceiveCall/ReceiveCall.js
+++ src/components/Modules/ReceiveCall/ReceiveCall.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import socket from "../../../utils/SocketIo";
-import { Container, Step } from "semantic-ui-react";
 import { DraftsTwoTone } from "@mui/icons-material";
 import ReceiveCallCSS from "./ReceiveCall.module.scss";
 import * as Twilio from "twilio-client";
+import { useImmer } from "use-immer";
+import CallProgress from "../../UI/CallProgress/CallProgress";
 
 const ReceiveCall = () => {
+  const [calls, setCalls] = useImmer({
+    calls: [],
+  });
+
   const [call, setCall] = useState();
   const [twilioToken, setTwilioToken] = useState();
 
@@ -20,51 +25,27 @@ const ReceiveCall = () => {
     socket.on("disconnect", () => {
       console.log("Socked disconnected");
     });
-    socket.on("call-new", (data) => {
-      console.log(data.data);
-      setCall(data.data);
-      //
+    socket.on("call-new", ({ data: { CallSid, CallStatus, From } }) => {
+      setCalls((draft) => {
+        draft.calls.push({ CallSid, CallStatus, From });
+      });
     });
-    socket.on("call-status-changed", (data) => {
-      console.log(data.data.status);
-      setCall(data.data.status);
-    });
-    socket.on("twilioToken", (data) => {
+    // socket.on("call-status-changed", (data) => {
+    //   console.log(data.data.status);
+    //   setCall(data.data.status);
+    // });
+    socket.on("twilio-token", (data) => {
       console.log("Received Token from the backend");
       console.log(data.token);
       setTwilioToken(data.token);
     });
-    socket.on("enqueue", (data) => {
-      let result = data;
-      console.log("enqueue socket data", result.data);
-      const CallSid = result.data.CallSid;
-      const CallStatus = "enqueue";
-      const Called = result.data.Called;
-      const Caller = result.data.Caller;
-      const From = result.data.From;
-      const To = result.data.To;
-
-      let updatedData = {
-        CallSid: CallSid,
-        CallStatus: CallStatus,
-        Called: Called,
-        Caller: Caller,
-        From: From,
-        To: To,
-      };
-
-      setCall(updatedData);
-      // setCall((data) => {
-      //   const index = data.findIndex(({ callSid }) => callSid === data.callSid);
-      //   data[index].data.CallStatus = "enqueue";
-      // });
-      // setCall((data) => {
-      //   // 3
-      //   // const index = data.findIndex(({ callSid }) => callSid === data.callSid);
-      //   console.log(data.data.CallStatus);
-      //   data.data.CallStatus = "enqueue";
-      //   console.log(data.data.CallStatus);
-      // });
+    socket.on("enqueue", ({ data: { CallSid } }) => {
+      setCalls((draft) => {
+        const index = draft.calls.findIndex(
+          ({ CallSid }) => CallSid === CallSid
+        );
+        draft.calls[index].CallStatus = "enqueue";
+      });
     });
     return () => {};
   }, []);
@@ -80,46 +61,15 @@ const ReceiveCall = () => {
     });
   }
 
-  const answerCall = (sid) => {
-    socket.emit("answer-call", { sid });
-  };
+  // const answerCall = (sid) => {
+  //   socket.emit("answer-call", { sid });
+  // };
 
   return (
     <div>
-      <div className={ReceiveCallCSS.container}>
-        {call && (
-          <Container
-            style={{ width: "620px", marginBottom: "110rem" }}
-            className={ReceiveCallCSS.container}
-          >
-            <Step.Group fluid>
-              <Step
-                icon="phone"
-                title="Ringing"
-                description={call.From}
-                active={call.CallStatus === "ringing"}
-                completed={call.CallStatus !== "ringing"}
-              />
-              <Step
-                icon="cogs"
-                title="In queue"
-                description="User waiting in queue"
-                active={call.CallStatus === "enqueue"}
-                disabled={call.CallStatus === "ringing"}
-                style={{ display: "inline" }}
-                onClick={() => answerCall(call.CallSid)}
-              />
-              <Step
-                icon="headphones"
-                title="Answered"
-                description="Answered by John"
-                disabled={call.CallStatus === "ringing"}
-              />
-              <Step icon="times" title="Hang up" description="Missed call" />
-            </Step.Group>
-          </Container>
-        )}
-      </div>
+      {calls.calls.map((call) => (
+        <CallProgress call={call} />
+      ))}
     </div>
   );
 };
---
+++ src/components/UI/CallProgress/CallProgress.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Container, Step } from "semantic-ui-react";
+// import "semantic-ui-css/semantic.min.css";
+import CallProgressCSS from "./CallProgress.module.scss";
+import socket from "../../../utils/SocketIo";
+
+const CallProgress = ({ call }) => {
+  function answerCall(sid) {
+    socket.emit("answer-call", { sid });
+  }
+  return (
+    <div className={CallProgressCSS.container}>
+      {
+        <Container
+          style={{ width: "620px", marginBottom: "110rem" }}
+          className={CallProgressCSS.container}
+        >
+          <Step.Group fluid>
+            <Step
+              icon="phone"
+              title="Ringing"
+              description={call.From}
+              active={call.CallStatus === "ringing"}
+              completed={call.CallStatus != "ringing"}
+            />
+            <Step
+              icon="cogs"
+              title="Pick Up"
+              //   description="User waiting in queue"
+              active={call.CallStatus === "enqueue"}
+              disabled={call.CallStatus === "ringing"}
+              style={{ display: "inline" }}
+              onClick={() => answerCall(call.CallSid)}
+            />
+            {/* <Step
+              icon="headphones"
+              title="Answered"
+              description="Answered by Admin"
+              disabled={
+                call.CallStatus === "ringing" || call.CallStatus === "enqueue"
+              }
+            /> */}
+            <Step
+              icon="times"
+              title="Hang up"
+              description="End the call"
+              onClick={() => window.location.reload()}
+            />
+          </Step.Group>
+        </Container>
+      }
+    </div>
+  );
+};
+
+export default CallProgress;
